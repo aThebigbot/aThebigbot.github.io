f85dd0b50c666ddb37383c3da14285025f7bb9e8
Thebigbot0000 <77632836+Thebigbot0000@users.noreply.github.com>
2021-10-16 19:15:18 +0200

    File

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-/* Nothing Code */
+function discord() {
+    window.open("https://discord.gg/3PXpRr2atg");
+}
+
+function npm() {
+    window.open("https://www.npmjs.com/~thebigbot");
+}
+
+function git() {
+    window.open("https://github.com/Thebigbot0000/");
+}
