2cbe88d55b3c5707c139e34bd035952b4464c998
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-07-03 10:05:48 +0200

    v3

---
+++ javascript/url.js
@@ -0,0 +1,10 @@
+const discord = document.getElementById("discord");
+const github = document.getElementById("github");
+
+discord.addEventListener("click", () => {
+  window.open("https://discord.com/users/703226153678405633");
+});
+
+github.addEventListener("click", () => {
+  window.open("https://github.com/aThebigbot");
+});
